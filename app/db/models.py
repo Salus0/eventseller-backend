a320adee2612433e939646ad50ea2aa082f30fef
Salus0 <s.ludwig.87@googlemail.com>
2026-08-03 19:54:33 +0200

    Update models.py

--- app/db/models.py
+++ app/db/models.py
@@ -1,1 +1,9 @@
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.orm import declarative_base
 
+Base = declarative_base()
+
+class Example(Base):
+    __tablename__ = "example"
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
