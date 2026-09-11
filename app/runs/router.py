44218ec11f3bee2ee144d7ffd43bca91cf259d7b
Salus0 <s.ludwig.87@googlemail.com>
2026-09-11 23:20:48 +0200

    Update router.py

--- app/runs/router.py
+++ app/runs/router.py
@@ -148,7 +148,7 @@ def get_runs():
     try:
         conn = psycopg2.connect(DATABASE_URL, cursor_factory=RealDictCursor)
         cur = conn.cursor()
-        cur.execute("SELECT * FROM runs WHERE status <> %s OR status IS NULL ORDER BY id DESC;", ("close",))
+        cur.execute("SELECT * FROM runs WHERE LOWER(status) <> 'close' OR status IS NULL ORDER BY id DESC;")
         runs = cur.fetchall()
         cur.close()
         conn.close()
